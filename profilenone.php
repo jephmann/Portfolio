1aabf0c61d62e500b93b781f0738f465c7444b2d
Jeffrey Hartmann <jephmann@gmail.com>
2011-07-29 11:09:59 -0500

    1st commit of cms/application/views/profilenone.php

---
+++ profilenone.php
@@ -0,0 +1,6 @@
+
+<div class="content">
+    <h1><?php echo $title; ?></h1>
+    <h3>You have no Profile in the System.<br/>Would you like to add yours?</h3>
+    <h3><?php echo anchor('profile/profileadd/','Add New Profile',array('class'=>'add')); ?></h3>
+</div>
