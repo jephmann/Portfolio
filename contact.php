d24f5bdada748a95bb0f67619e7b12586f99b790
Jeffrey Hartmann <jephmann@gmail.com>
2011-07-29 09:45:49 -0500

    first commit of (yet to code) contact.php for portfolio page Contact Me form posts

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    echo "<p>Reserved for e-mailing data from the contact form.
+        Although suggested by the form's action in the code,
+        this file was not included in the original project.</p>
+        <p>If the user wishes Contact Me to appear on the portfolio,
+        user must supply a Contact email address.</p>
+        <p>Server-side validation may be included in this file
+        in the event that client-side (javascript) validation is blocked.
+        Original project does not seem to require any form fields;
+        at least an e-mail address should be required.</p>
+        <p>Do not forget to copy and include PHPMail scripts for this process.</p>
+        <p>Last but not least, what happens after the email is sent?
+        Redirect to index?</p>
+        <p>ENOUGH WITH THE NOTES! Please Click BACK.</p>
+        <p>(And, having clicked Back, the form disappears, making validation trickier.)</p>
+        <p>This file might create session variables based on the input data,
+        which would hold their values until either the upper-right X is clicked
+        for cancellation or until the email is successfully sent.</p>"
+?>
