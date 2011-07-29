106cc69b9978f7a402ea8069346aaa7954f19004
Jeffrey Hartmann <jephmann@gmail.com>
2011-07-29 11:08:28 -0500

    1st commit of cms/application/views/profilelist.php

---
+++ profilelist.php
@@ -0,0 +1,6 @@
+
+        <div class="content">
+            <h1><?php echo $title; ?></h1>
+            <div class="paging"><?php echo $pagination; ?></div>
+            <div class="data"><?php echo $table; ?></div>
+        </div>
