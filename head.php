5091372781cecfd7b0648572612c0514faf25d1e
Jeffrey Hartmann <jephmann@gmail.com>
2011-07-29 10:24:13 -0500

    1st commit of cms/application/views/head.php

---
+++ head.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Portfolio Management: <?php echo $title; ?></title>
+    </head>
+    <body>
