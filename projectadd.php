ce24a57fee5146133fb972ac994134317ac6678f
Jeffrey Hartmann <jephmann@gmail.com>
2011-07-29 10:42:03 -0500

    1st commit of cms/application/views/projectadd.php

---
+++ projectadd.php
@@ -0,0 +1,40 @@
+
+        <div class="content">
+            <h1><?php echo $title; ?></h1>
+            <?php echo $message; ?>
+            <?php echo form_open_multipart($action); ?>
+            <div class="data">
+                <table>
+                    <tr>
+                        <td valign="top">Project Title</td>
+                        <td>
+                            <input type="text" name="title" class="text"
+                                   value="<?php echo set_value('title'); ?>"/>
+                                   <?php echo form_error('title') ?> 
+                        </td>
+                    </tr>
+                    <tr>
+                        <td valign="top">Project Description</td>
+                        <td>
+                            <textarea name="description" class="text"><?php echo set_value('description'); ?></textarea>
+                                   <?php echo form_error('description') ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td valign="top">Link to Work</td>
+                        <td>
+                            <input type="text" name="urlwork" class="text"
+                                   value="<?php echo set_value('urlwork'); ?>"/>
+                                   <?php echo form_error('urlwork') ?>
+                        </td>
+                    </tr>                    
+                    <tr>
+                        <td><input type="hidden" name="idproject"
+                           value="<?php echo set_value('idproject') ?>"/></td>
+                        <td><input type="submit" value="Save"/></td>
+                    </tr>
+                </table>
+            </div>
+        </form>
+        <br /><?php echo $link_back; ?>
+        </div>
