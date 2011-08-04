7d2dfab528f3940776d88f576be7d8487fef10d8
Jeffrey Hartmann <jephmann@gmail.com>
2011-08-04 11:47:44 -0500

    provision for $morecss variable in head tag

--- head.php
+++ head.php
@@ -3,5 +3,9 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Portfolio Management: <?php echo $title; ?></title>
+        <link href="<?=base_url();?>css/portfolio.css" media="screen" rel="stylesheet" type="text/css" />
+        <link href="<?=base_url();?>css/links.css" media="screen" rel="stylesheet" type="text/css" />
+        <?=$morecss ?>
     </head>
     <body>
+        <div class="page">
