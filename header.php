ca637e01ccdc080c42b23a22ad1d9c13a64dbaf7
Jeffrey Hartmann <jephmann@gmail.com>
2011-08-21 19:12:20 -0500

    miscellaneous tweaks

--- header.php
+++ header.php
@@ -1,13 +1,32 @@
-
-<div class="content">
-    <div id="header">
-        <div class="left">
-            <h1><?=$this->session->userdata('profilenamefirst'); ?><span><?=$this->session->userdata('profilenamelast'); ?></span></h1>
-            <?=$this->session->userdata('urllinkedin'); ?>&nbsp;<?=$this->session->userdata('fileresume'); ?>&nbsp;<?=$this->session->userdata('contactemail'); ?>
-        </div>
-        <div class="right">
-            <h2>WELCOME TO THE PORTFOLIO OF <?=$this->session->userdata('profilenamefirst').'&nbsp;'.$this->session->userdata('profilenamelast'); ?></h2>
-        </div>
+<?php
+    $ssnurllinkedin=$this->session->userdata('urllinkedin');
+    if(strlen(trim($ssnurllinkedin))==0){
+        $ssnurllinkedin="No";
+    }
+    $ssnfileresume=$this->session->userdata('fileresume');
+    if(strlen(trim($ssnfileresume))==0){
+        $ssnfileresume="No";
+    }
+    $ssncontactemail=$this->session->userdata('contactemail');
+    if(strlen(trim($ssncontactemail))==0){
+        $ssncontactemail="No";
+    }
+?>
+<header>
+    <div class="left">
+        <h1><a><?=$this->session->userdata('profilenamefirst'); ?><span><?=$this->session->userdata('profilenamelast'); ?></span></a></h1>
     </div>
-    <br /><?=anchor('profile/profileupdate/', 'Update Your Profile',array('class'=>'update')); ?> | <?=anchor('profile/resumeupdate/', 'Update Your Resume',array('class'=>'update')); ?> | <?=$this->session->userdata('headermessage'); ?>
-</div>
+    <div class="right">
+        <ul class="ulistdisc">
+            <li>LinkedIn:&nbsp;<?=$ssnurllinkedin ?></li>
+            <li>Resume:&nbsp;<?=$ssnfileresume ?></li>
+            <li>E-Mail (Contact Me):&nbsp;<?=$ssncontactemail ?></li>
+        </ul>
+    </div>
+    <div class="navmsg">
+        <p class="linkback">
+        <?=anchor('profile/logout/', 'Log Out',array('class'=>'back')); ?> | <?=anchor('profile/profileupdate/', 'Update Your Profile',array('class'=>'update')); ?> | <?=anchor('profile/resumeupdate/', 'Update Your Resume',array('class'=>'update')); ?> | <?=$this->session->userdata('headermessage'); ?>
+        </p>
+    </div>
+</header>
+<div class="clear"></div>
