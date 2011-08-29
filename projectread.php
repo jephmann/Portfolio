86e3cbf160ade5a9d86aef943bc8fe7f0a234adb
Jeffrey Hartmann <jephmann@gmail.com>
2011-08-29 10:39:53 -0500

    view tweaks

--- projectread.php
+++ projectread.php
@@ -1,45 +1,37 @@
-
-        <div class="content">
-            <h1><?php echo $title; ?></h1>
-            <div class="data">
-                <table>
-                    <tr>
-                        <td valign="top">Title</td>
-                        <td><?php echo $project->title; ?></td>
-                    </tr>
-                    <tr>
-                        <td valign="top">Description</td>
-                        <td><?php echo $project->description; ?></td>
-                    </tr>
-                    <tr>
-                        <td valign="top">Link to Work</td>
-                        <td><?php echo $project->urlwork; ?></td>
-                    </tr>
-                    <tr>
-                        <td valign="top">Image Left</td>
-                        <td><?php echo $imageleft; ?></td>
-                    </tr>
-                    <tr>
-                        <td valign="top">Description</td>
-                        <td><?php echo $project->altleft; ?></td>
-                    </tr>
-                    <tr>
-                        <td valign="top">Image Right Top</td>
-                        <td><?php echo $imagerighttop; ?></td>
-                    </tr>
-                    <tr>
-                        <td valign="top">Description</td>
-                        <td><?php echo $project->altrighttop; ?></td>
-                    </tr>
-                    <tr>
-                        <td valign="top">Image Right Bottom</td>
-                        <td><?php echo $imagerightbottom; ?></td>
-                    </tr>
-                    <tr>
-                        <td valign="top">Description</td>
-                        <td><?php echo $project->altrightbottom; ?></td>
-                    </tr>
-                </table>
-            </div>
-            <br /><?php echo $link_back2 ?>&nbsp;<?php echo $link_back; ?>
+<div class="projectread">
+    <div class="top">
+        <div class="topleft">
+            <h1><?=$title; ?></h1>
         </div>
+        <div class="topright">
+            <p class="linkback"><?=$link_back2 ?>&nbsp;<?=$link_back; ?></p>
+        </div>
+    </div>
+    <div class="clear"></div>
+    <div class="data">
+        <div class="row">
+            <label class="lblleft">Title</label>
+            <label class="lblright"><?=$project->title; ?></label>
+        </div>
+        <div class="row">
+            <label class="lblleft">Description</label>
+            <label class="lblright"><?=$project->description; ?></label>
+        </div>
+        <div class="row">
+            <label class="lblleft">Link to Work</label>
+            <label class="lblright"><?=$project->urlwork; ?></label>
+        </div>
+        <div class="row">
+            <label class="lblleft">Image Left</label>
+            <label class="lblright"><?=$imageleft; ?><?=$project->altleft; ?></label>
+        </div>
+        <div class="row">
+            <label class="lblleft">Image Right Top</label>
+            <label class="lblright"><?=$imagerighttop; ?><?=$project->altrighttop; ?></label>
+        </div>
+        <div class="row">
+            <label class="lblleft">Image Right Bottom</label>
+            <label class="lblright"><?=$imagerightbottom; ?><?=$project->altrightbottom; ?></label>
+        </div>
+    </div>
+</div>
